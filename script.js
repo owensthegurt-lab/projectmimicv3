7074e8f9fe648a3da581747e10961aecac3d4643
thegurt <owensthegurt@gmail.com>
2026-07-16 19:59:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
+```javascript id="w8xk3f"
+/*
+========================================
+PROJECT MIMIC
+Main Menu
+========================================
+*/
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const playButton = document.getElementById("play");
+    const settingsButton = document.getElementById("settings");
+    const creditsButton = document.getElementById("credits");
+    const quitButton = document.getElementById("quit");
+
+    if (playButton) {
+        playButton.addEventListener("click", startGame);
+    }
+
+    if (settingsButton) {
+        settingsButton.addEventListener("click", openSettings);
+    }
+
+    if (creditsButton) {
+        creditsButton.addEventListener("click", openCredits);
+    }
+
+    if (quitButton) {
+        quitButton.addEventListener("click", quitGame);
+    }
+
+});
+
+function startGame() {
+
+    document.body.style.transition = "opacity .5s ease";
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+
+        window.location.href = "game.html";
+
+    }, 500);
+
+}
+
+function openSettings() {
+
+    alert("Settings are coming soon.");
+
+}
+
+function openCredits() {
+
+    alert(
+`PROJECT MIMIC
+
+Created by Owens
+
+Powered by HTML, CSS & JavaScript
+
+More credits will be added during development.`
+    );
+
+}
+
+function quitGame() {
+
+    alert("Close this tab to quit Project Mimic.");
+
+}
+```
 
