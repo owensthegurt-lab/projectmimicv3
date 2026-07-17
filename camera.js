0d1adb8fa1c58c6a7bd8bfe29be840e1ef2e1b0e
thegurt <owensthegurt@gmail.com>
2026-07-16 21:44:22 -0400

    Update camera.js

--- camera.js
+++ camera.js
@@ -11,12 +11,18 @@ export class Camera {
         this.x = 0;
         this.y = 0;
 
+        // Smooth camera
+        this.followSpeed = 8;
+
     }
 
-    update(player, canvas) {
+    update(player, canvas, delta) {
 
-        this.x = player.worldX - canvas.width / 2;
-        this.y = player.worldY - canvas.height / 2;
+        const targetX = player.worldX - canvas.width / 2;
+        const targetY = player.worldY - canvas.height / 2;
+
+        this.x += (targetX - this.x) * this.followSpeed * delta;
+        this.y += (targetY - this.y) * this.followSpeed * delta;
 
     }
 
