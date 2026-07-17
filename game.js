235487cf79068576290ed8fb4011800d615cbd60
thegurt <owensthegurt@gmail.com>
2026-07-17 14:05:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,11 +15,18 @@ import { World } from "./world.js";
 
 import { Interaction } from "./interaction.js";
 
+import { MimicManager } from "./mimicManager.js";
+
 class Game {
 
     constructor() {
 
-        // Canvas
+        /*
+        ==========================
+        CANVAS
+        ==========================
+        */
+
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
 
@@ -30,19 +37,49 @@ class Game {
             () => this.resize()
         );
 
-        // Engine
+        /*
+        ==========================
+        ENGINE
+        ==========================
+        */
+
         this.input = new Input();
         this.mouse = new Mouse(this.canvas);
 
         this.camera = new Camera();
+
         this.renderer = new Renderer(
+
             this.ctx,
             this.canvas
+
         );
 
-        // World
+        /*
+        ==========================
+        WORLD
+        ==========================
+        */
+
         this.world = new World();
+
         this.player = new Player();
+
+        /*
+        ==========================
+        MIMIC
+        ==========================
+        */
+
+        this.mimicManager = new MimicManager(
+            this.world
+        );
+
+        /*
+        ==========================
+        GAME LOOP
+        ==========================
+        */
 
         this.lastTime = performance.now();
 
@@ -77,26 +114,65 @@ class Game {
 
     update(delta) {
 
-        // Player
+        /*
+        ==========================
+        PLAYER
+        ==========================
+        */
+
         this.player.update(
+
             delta,
+
             this.input,
             this.mouse,
+
             this.world
+
         );
 
-        // Interactions
+        /*
+        ==========================
+        INTERACTION
+        ==========================
+        */
+
         Interaction.update(
+
             this.player,
             this.world,
             this.input
+
         );
 
-        // Camera
+        /*
+        ==========================
+        MIMIC
+        ==========================
+        */
+
+        this.mimicManager.update(
+
+            delta,
+
+            this.player
+
+        );
+
+        /*
+        ==========================
+        CAMERA
+        ==========================
+        */
+
         this.camera.update(
+
             this.player,
+
             this.canvas,
+
             delta
+
         );
 
     }
@@ -104,9 +180,15 @@ class Game {
     render() {
 
         this.renderer.render(
+
             this.world,
+
             this.player,
-            this.camera
+
+            this.camera,
+
+            this.mimicManager
+
         );
 
     }
