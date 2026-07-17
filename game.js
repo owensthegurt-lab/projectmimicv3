40f570206110181de33982688645586bcd2eae0d
thegurt <owensthegurt@gmail.com>
2026-07-16 20:21:44 -0400

    Update background color and circle size in game.js
    Changed background color to red and updated circle size.

--- game.js
+++ game.js
@@ -4,18 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Black background
-ctx.fillStyle = "black";
+ctx.fillStyle = "red";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-// White circle
 ctx.fillStyle = "white";
 ctx.beginPath();
-ctx.arc(200, 200, 30, 0, Math.PI * 2);
+ctx.arc(200, 200, 40, 0, Math.PI * 2);
 ctx.fill();
 
-// White text
-ctx.font = "30px Arial";
-ctx.fillText("PROJECT MIMIC", 250, 210);
-
-console.log("Canvas:", canvas.width, canvas.height);
+console.log("game.js loaded");
