7086adefb3830976e05416c9eb858f7fec1be10c
thegurt <owensthegurt@gmail.com>
2026-07-17 13:02:39 -0400

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -21,7 +21,6 @@ export class Renderer {
 
         const ctx = this.ctx;
 
-        // Clear Screen
         ctx.clearRect(
             0,
             0,
@@ -29,27 +28,37 @@ export class Renderer {
             this.canvas.height
         );
 
-        // Draw World
         world.draw(
+
             ctx,
+
             camera.x,
             camera.y,
-            this.canvas.width,
-            this.canvas.height
-        );
 
-        // Draw Player
-        player.draw(
-            ctx,
             this.canvas.width,
-            this.canvas.height
-        );
+            this.canvas.height,
 
-        // Draw Lighting LAST
-        this.lighting.draw(
-            ctx,
-            this.canvas,
             player
+
+        );
+
+        player.draw(
+
+            ctx,
+
+            this.canvas.width,
+            this.canvas.height
+
+        );
+
+        this.lighting.draw(
+
+            ctx,
+
+            this.canvas,
+
+            player
+
         );
 
     }
