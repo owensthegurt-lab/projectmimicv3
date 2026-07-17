bbec691163e0bd710cd36bca1a8c0915ef9e94c8
thegurt <owensthegurt@gmail.com>
2026-07-16 20:07:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
 window.onload = function () {
 
     const play = document.getElementById("play");
-    const settings = document.getElementById("settings");
-    const credits = document.getElementById("credits");
-    const quit = document.getElementById("quit");
 
-    play.addEventListener("click", function () {
+    play.onclick = function () {
 
         window.location.href = "game.html";
 
-    });
-
-    settings.addEventListener("click", function () {
-
-        alert("Settings Coming Soon");
-
-    });
-
-    credits.addEventListener("click", function () {
-
-        alert("Project Mimic\nCreated by Owens");
-
-    });
-
-    quit.addEventListener("click", function () {
-
-        alert("Close the tab to quit.");
-
-    });
+    };
 
 };
