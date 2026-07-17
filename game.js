ab7158a11ab7a21abf92bc2b2ccede7958921297
thegurt <owensthegurt@gmail.com>
2026-07-16 21:19:41 -0400

    Update game.js

--- game.js
+++ game.js
@@ -15,24 +15,22 @@ class Game {
         this.canvas = document.getElementById("game");
         this.ctx = this.canvas.getContext("2d");
 
-        this.keys = {};
-
         this.resize();
 
         window.addEventListener("resize", () => this.resize());
 
+        // Keyboard
+        this.keys = {};
+
         window.addEventListener("keydown", (e) => {
-
             this.keys[e.key.toLowerCase()] = true;
-
         });
 
         window.addEventListener("keyup", (e) => {
-
             this.keys[e.key.toLowerCase()] = false;
-
         });
 
+        // Game Objects
         this.world = new World();
         this.player = new Player();
         this.flashlight = new Flashlight();
@@ -53,7 +51,6 @@ class Game {
     loop(time) {
 
         const delta = (time - this.lastTime) / 1000;
-
         this.lastTime = time;
 
         this.update(delta);
@@ -65,23 +62,17 @@ class Game {
 
     update(delta) {
 
-        this.player.update(
-            delta,
-            this.keys,
-            this.world
-        );
+        this.player.update(delta, this.keys);
 
     }
 
     render() {
 
         const cameraX =
-            this.player.worldX -
-            this.canvas.width / 2;
+            this.player.worldX - this.canvas.width / 2;
 
         const cameraY =
-            this.player.worldY -
-            this.canvas.height / 2;
+            this.player.worldY - this.canvas.height / 2;
 
         // Draw World
         this.world.draw(
@@ -99,7 +90,7 @@ class Game {
             this.canvas.height
         );
 
-        // Draw Flashlight Overlay
+        // Flashlight
         this.flashlight.draw(
             this.ctx,
             this.canvas.width,
