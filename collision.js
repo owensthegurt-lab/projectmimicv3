1ed14d53b67340c00bd43eb2c924f4529db3a744
thegurt <owensthegurt@gmail.com>
2026-07-16 20:53:49 -0400

    Update collision.js

--- collision.js
+++ collision.js
@@ -1,1 +1,34 @@
+/*
+====================================
+COLLISION
+====================================
+*/
 
+class CollisionSystem {
+
+    static checkPlayer(player, world) {
+
+        for (const building of world.buildings) {
+
+            const padding = player.radius;
+
+            if (
+
+                player.worldX + padding > building.x &&
+                player.worldX - padding < building.x + building.width &&
+                player.worldY + padding > building.y &&
+                player.worldY - padding < building.y + building.height
+
+            ) {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+}
