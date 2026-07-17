3619a0ce292ca034601da2d3acb4126c84ff2de7
thegurt <owensthegurt@gmail.com>
2026-07-16 20:44:38 -0400

    Update world.js

--- world.js
+++ world.js
@@ -10,15 +10,55 @@ class World {
 
         this.tileSize = 64;
 
-        this.width = 50;
-        this.height = 50;
+        this.width = 100;
+        this.height = 100;
+
+        this.buildings = [
+
+            // Player House
+            {
+                x: 1500,
+                y: 1450,
+                width: 220,
+                height: 180,
+                color: "#8b5a2b"
+            },
+
+            // NPC Building 1
+            {
+                x: 900,
+                y: 1100,
+                width: 180,
+                height: 220,
+                color: "#555"
+            },
+
+            // NPC Building 2
+            {
+                x: 2300,
+                y: 1300,
+                width: 220,
+                height: 160,
+                color: "#666"
+            },
+
+            // NPC Building 3
+            {
+                x: 1800,
+                y: 2200,
+                width: 200,
+                height: 200,
+                color: "#777"
+            }
+
+        ];
 
     }
 
     draw(ctx, cameraX, cameraY, canvasWidth, canvasHeight) {
 
-        // Background
-        ctx.fillStyle = "#1a1a1a";
+        // Grass
+        ctx.fillStyle = "#355e3b";
         ctx.fillRect(
             0,
             0,
@@ -26,43 +66,47 @@ class World {
             canvasHeight
         );
 
-        // Grid
-        ctx.strokeStyle = "#2d2d2d";
-        ctx.lineWidth = 1;
+        // Roads
+        ctx.fillStyle = "#444";
 
-        for (let x = 0; x <= this.width; x++) {
+        // Horizontal road
+        ctx.fillRect(
+            -cameraX,
+            1550 - cameraY,
+            this.width * this.tileSize,
+            120
+        );
 
-            ctx.beginPath();
+        // Vertical road
+        ctx.fillRect(
+            1600 - cameraX,
+            -cameraY,
+            120,
+            this.height * this.tileSize
+        );
 
-            ctx.moveTo(
-                x * this.tileSize - cameraX,
-                -cameraY
+        // Buildings
+        for (const building of this.buildings) {
+
+            ctx.fillStyle = building.color;
+
+            ctx.fillRect(
+                building.x - cameraX,
+                building.y - cameraY,
+                building.width,
+                building.height
             );
 
-            ctx.lineTo(
-                x * this.tileSize - cameraX,
-                this.height * this.tileSize - cameraY
+            // Roof outline
+            ctx.strokeStyle = "#222";
+            ctx.lineWidth = 4;
+
+            ctx.strokeRect(
+                building.x - cameraX,
+                building.y - cameraY,
+                building.width,
+                building.height
             );
-
-            ctx.stroke();
-
-        }
-
-        for (let y = 0; y <= this.height; y++) {
-
-            ctx.beginPath();
-
-            ctx.moveTo(
-                -cameraX,
-                y * this.tileSize - cameraY
-            );
-
-            ctx.lineTo(
-                this.width * this.tileSize - cameraX,
-                y * this.tileSize - cameraY
-            );
-
-            ctx.stroke();
 
         }
 
