782916fc2c5a35c8a0d253a2d668ac05b9debae2
thegurt <owensthegurt@gmail.com>
2026-07-17 15:00:44 -0400

    Update interior.js

--- interior.js
+++ interior.js
@@ -6,103 +6,223 @@ INTERIOR
 
 export class Interior {
 
-    constructor(width, height) {
 
-        this.width = width;
-        this.height = height;
+constructor(width, height) {
 
-        this.walls = [];
-        this.furniture = [];
 
-        // Outer walls
+    this.width = width;
 
-        this.addWall(0, 0, width, 12);
-        this.addWall(0, height - 12, width, 12);
+    this.height = height;
 
-        this.addWall(0, 0, 12, height);
-        this.addWall(width - 12, 0, 12, height);
 
+    this.walls = [];
+
+
+    /*
+    ====================================
+    LOOT READY
+    ====================================
+
+    Future loot system will use this.
+
+    Example:
+
+    {
+        x:100,
+        y:80,
+        type:"battery"
     }
 
-    addWall(x, y, width, height) {
+    */
 
-        this.walls.push({
+    this.loot = [];
 
-            x,
-            y,
-            width,
-            height
 
-        });
 
-    }
+    /*
+    ====================================
+    OUTER WALLS
+    ====================================
+    */
 
-    addFurniture(x, y, width, height, color) {
 
-        this.furniture.push({
+    this.addWall(
 
-            x,
-            y,
-            width,
-            height,
-            color
+        0,
+        0,
 
-        });
+        width,
+        12
 
-    }
+    );
 
-    draw(ctx, cameraX, cameraY, originX, originY) {
 
-        // Floor
+    this.addWall(
 
-        ctx.fillStyle = "#8A7A63";
+        0,
+        height - 12,
+
+        width,
+        12
+
+    );
+
+
+    this.addWall(
+
+        0,
+        0,
+
+        12,
+        height
+
+    );
+
+
+    this.addWall(
+
+        width - 12,
+        0,
+
+        12,
+        height
+
+    );
+
+
+}
+
+
+
+
+
+/*
+====================================
+WALLS
+====================================
+*/
+
+addWall(x, y, width, height) {
+
+
+    this.walls.push({
+
+        x,
+
+        y,
+
+        width,
+
+        height
+
+    });
+
+
+}
+
+
+
+
+
+/*
+====================================
+LOOT
+====================================
+*/
+
+addLoot(x, y, type) {
+
+
+    this.loot.push({
+
+        x,
+
+        y,
+
+        type
+
+    });
+
+
+}
+
+
+
+
+
+/*
+====================================
+DRAW
+====================================
+*/
+
+draw(ctx, cameraX, cameraY, originX, originY) {
+
+
+
+    /*
+    FLOOR
+    */
+
+
+    ctx.fillStyle = "#8A7A63";
+
+
+    ctx.fillRect(
+
+        originX - cameraX,
+
+        originY - cameraY,
+
+        this.width,
+
+        this.height
+
+    );
+
+
+
+    /*
+    LOOT
+
+    Hidden for now.
+    We will add proper loot
+    rendering later.
+
+    */
+
+
+
+    /*
+    WALLS
+    */
+
+
+    ctx.fillStyle = "#5D4A37";
+
+
+
+    for (const wall of this.walls) {
+
 
         ctx.fillRect(
 
-            originX - cameraX,
-            originY - cameraY,
+            originX + wall.x - cameraX,
 
-            this.width,
-            this.height
+            originY + wall.y - cameraY,
+
+            wall.width,
+
+            wall.height
 
         );
 
-        // Furniture
-
-        for (const item of this.furniture) {
-
-            ctx.fillStyle = item.color;
-
-            ctx.fillRect(
-
-                originX + item.x - cameraX,
-                originY + item.y - cameraY,
-
-                item.width,
-                item.height
-
-            );
-
-        }
-
-        // Walls
-
-        ctx.fillStyle = "#5D4A37";
-
-        for (const wall of this.walls) {
-
-            ctx.fillRect(
-
-                originX + wall.x - cameraX,
-                originY + wall.y - cameraY,
-
-                wall.width,
-                wall.height
-
-            );
-
-        }
 
     }
+
+
+}
+
+
 
 }
