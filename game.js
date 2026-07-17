1d7d32db9837ae1b4362ff7646ff68665108ee7b
thegurt <owensthegurt@gmail.com>
2026-07-16 20:42:42 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,61 +4,6 @@ PROJECT MIMIC
 ====================================
 */
 
-class Player {
-
-    constructor() {
-
-        this.x = window.innerWidth / 2;
-        this.y = window.innerHeight / 2;
-
-        this.radius = 15;
-        this.speed = 300;
-
-    }
-
-    update(delta, keys) {
-
-        if (keys["w"]) this.y -= this.speed * delta;
-        if (keys["s"]) this.y += this.speed * delta;
-        if (keys["a"]) this.x -= this.speed * delta;
-        if (keys["d"]) this.x += this.speed * delta;
-
-    }
-
-    draw(ctx) {
-
-        // Shadow
-        ctx.fillStyle = "rgba(0,0,0,.3)";
-
-        ctx.beginPath();
-        ctx.ellipse(
-            this.x,
-            this.y + 12,
-            14,
-            6,
-            0,
-            0,
-            Math.PI * 2
-        );
-        ctx.fill();
-
-        // Player
-        ctx.fillStyle = "white";
-
-        ctx.beginPath();
-        ctx.arc(
-            this.x,
-            this.y,
-            this.radius,
-            0,
-            Math.PI * 2
-        );
-        ctx.fill();
-
-    }
-
-}
-
 class Game {
 
     constructor() {
@@ -83,6 +28,8 @@ class Game {
             this.keys[e.key.toLowerCase()] = false;
 
         });
+
+        this.world = new World();
 
         this.player = new Player();
 
@@ -115,47 +62,26 @@ class Game {
 
     update(delta) {
 
-        this.player.update(delta, this.keys);
+        this.player.update(
+            delta,
+            this.keys
+        );
 
     }
 
     render() {
 
-        // Background
-        this.ctx.fillStyle = "#1a1a1a";
-        this.ctx.fillRect(
+        this.world.draw(
+            this.ctx,
             0,
             0,
             this.canvas.width,
             this.canvas.height
         );
 
-        // Grid
-        this.ctx.strokeStyle = "#2d2d2d";
-        this.ctx.lineWidth = 1;
-
-        const grid = 64;
-
-        for (let x = 0; x < this.canvas.width; x += grid) {
-
-            this.ctx.beginPath();
-            this.ctx.moveTo(x, 0);
-            this.ctx.lineTo(x, this.canvas.height);
-            this.ctx.stroke();
-
-        }
-
-        for (let y = 0; y < this.canvas.height; y += grid) {
-
-            this.ctx.beginPath();
-            this.ctx.moveTo(0, y);
-            this.ctx.lineTo(this.canvas.width, y);
-            this.ctx.stroke();
-
-        }
-
-        // Player
-        this.player.draw(this.ctx);
+        this.player.draw(
+            this.ctx
+        );
 
     }
 
