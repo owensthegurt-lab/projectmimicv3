819f7fee4541e6f9a479b19dbc55fad837d26a33
thegurt <owensthegurt@gmail.com>
2026-07-16 20:19:10 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,131 +1,21 @@
-/*
-====================================
-PROJECT MIMIC
-====================================
-*/
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-class Player {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    constructor() {
+// Black background
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        this.x = 400;
-        this.y = 300;
+// White circle
+ctx.fillStyle = "white";
+ctx.beginPath();
+ctx.arc(200, 200, 30, 0, Math.PI * 2);
+ctx.fill();
 
-        this.speed = 250;
+// White text
+ctx.font = "30px Arial";
+ctx.fillText("PROJECT MIMIC", 250, 210);
 
-    }
-
-    update(delta, keys) {
-
-        if (keys["w"])
-            this.y -= this.speed * delta;
-
-        if (keys["s"])
-            this.y += this.speed * delta;
-
-        if (keys["a"])
-            this.x -= this.speed * delta;
-
-        if (keys["d"])
-            this.x += this.speed * delta;
-
-    }
-
-    draw(ctx) {
-
-        ctx.fillStyle = "white";
-
-        ctx.beginPath();
-
-        ctx.arc(
-            this.x,
-            this.y,
-            12,
-            0,
-            Math.PI * 2
-        );
-
-        ctx.fill();
-
-    }
-
-}
-
-class Game {
-
-    constructor() {
-
-        this.canvas = document.getElementById("game");
-        this.ctx = this.canvas.getContext("2d");
-
-        this.resize();
-
-        window.addEventListener("resize", () => this.resize());
-
-        this.keys = {};
-
-        window.addEventListener("keydown", (e) => {
-
-            this.keys[e.key.toLowerCase()] = true;
-
-        });
-
-        window.addEventListener("keyup", (e) => {
-
-            this.keys[e.key.toLowerCase()] = false;
-
-        });
-
-        this.player = new Player();
-
-        this.lastTime = performance.now();
-
-        requestAnimationFrame(this.loop.bind(this));
-
-    }
-
-    resize() {
-
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-
-    }
-
-    loop(time) {
-
-        const delta = (time - this.lastTime) / 1000;
-
-        this.lastTime = time;
-
-        this.update(delta);
-
-        this.render();
-
-        requestAnimationFrame(this.loop.bind(this));
-
-    }
-
-    update(delta) {
-
-        this.player.update(delta, this.keys);
-
-    }
-
-    render() {
-
-        this.ctx.fillStyle = "#050505";
-
-        this.ctx.fillRect(
-            0,
-            0,
-            this.canvas.width,
-            this.canvas.height
-        );
-
-        this.player.draw(this.ctx);
-
-    }
-
-}
-
-new Game();
+console.log("Canvas:", canvas.width, canvas.height);
